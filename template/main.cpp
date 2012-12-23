d2671175a11970c242a2218de20f6ffc8963fda4
Dwain Faithfull <dwfaithfull@gmail.com>
2012-12-23 07:32:43 +0000

    Add script to create structure for problem dirs

---
+++ template/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdexcept>
+#include <rosalind.h>
+
+void process_data(std::vector<std::string> data);
+
+int main(int argc, char** argv)
+{
+  if (argc != 2)
+  {
+    std::cout << "Usage: TARGET <input file path>" << std::endl;
+    return -1;
+  }
+
+  auto data = Rosalind::read_data_file(argv[1]);
+
+  unsigned int expected_data_lines = 1;
+  if (data.size() != expected_data_lines)
+  {
+    std::cout << "Expected " << expected_data_lines
+      << "lines in data file." << std::endl;
+    return -1;
+  }
+
+  process_data(data);
+
+  return 0;
+}
+
+
+void process_data(std::vector<std::string> data)
+{
+
+}
